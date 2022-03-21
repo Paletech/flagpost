f5c435c260f64e88e4d8da583ffafe048fa5ab36
Havshyn Maksym <maksgavshin@gmail.com>
2022-03-21 16:54:02 +0200

    tests

--- backend/app/files/models.py
+++ backend/app/files/models.py
@@ -1,19 +1,25 @@
-import datetime
-
-from sqlalchemy import Boolean, Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy_utils import ColorType
-from app.db.session import Base
-
-
-# class Images(Base):
-#     __tablename__ = "image"
+# import datetime
+# import uuid
 #
-#     id = Column(Integer, primary_key=True, index=True, autoincrement=True, unique=True)
-#     user_id = Column(Integer, ForeignKey("user.id"))
-#     category_id = Column(Integer, ForeignKey("category.id"))
+# from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+# from sqlalchemy.dialects.postgresql import UUID
+#
+# from app.db.session import Base
+#
+#
+# def generate_uuid():
+#     return str(uuid.uuid4())
+#
+#
+# class Files(Base):
+#     __tablename__ = "file"
+#
+#     id = Column(UUID(as_uuid=True), primary_key=True, default=generate_uuid, unique=True)
 #     width = Column(Integer)
 #     height = Column(Integer)
 #     path = Column(String)
 #     public_path = Column(String)
 #     created_at = Column(DateTime, default=datetime.datetime.utcnow)
 #     updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+#
+#     post_id = Column(UUID(as_uuid=True), ForeignKey("post.id"))
