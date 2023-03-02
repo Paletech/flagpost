e3d4bba37d0f56d40ccfa19ebdc94a2158367e60
OleksandrPrudyvus <pryduvyso20@gmail.com>
2023-03-02 17:08:20 +0200

    Created a test for routers that create and delete posts

--- backend/app/api/api_v1/routers/tests/test_posts.py
+++ backend/app/api/api_v1/routers/tests/test_posts.py
@@ -11,3 +11,22 @@ def test_retrieve_post_with_id(client, user_token_headers, test_post):
     """Test for retrieve post from id."""
     response = client.get(f"/api/v1/posts/{test_post.id}", headers=user_token_headers)
     assert response.status_code == 200
+
+
+def test_create_post(client, user_token_headers, test_user):
+    """Test for creating new post."""
+    post = {
+        "type": "type",
+        "text": "text",
+        "user_id": str(test_user.id),
+        "created_at": "2023-03-02 16:05:57.312334",
+    }
+    response = client.post('/api/v1/posts', headers=user_token_headers, json=post)
+
+    assert response.status_code == 200
+
+
+def test_delete_post(client, superuser_token_headers, test_post):
+    """Test for deleting post from id."""
+    response = client.delete(f"/api/v1/posts/{test_post.id}", headers=superuser_token_headers)
+    assert response.status_code == 200
