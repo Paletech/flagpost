10827dddc49dc667d8780d3ec47a7baf934bb87c
Oleksandr Prudyvus <71883661+OleksandrPrudyvus@users.noreply.github.com>
2023-03-15 12:56:58 +0200

    Changed BASE_URL

--- frontend/src/config/index.tsx
+++ frontend/src/config/index.tsx
@@ -1,3 +1,3 @@
-export const BASE_URL: string = 'http://localhost:8000';
+export const BASE_URL: string = 'https://s6mzej3awpgn75k65rbrrdrqsy0xoylp.lambda-url.us-east-1.on.aws';
 export const BACKEND_URL: string =
   'https://s6mzej3awpgn75k65rbrrdrqsy0xoylp.lambda-url.us-east-1.on.aws/api/v1';
