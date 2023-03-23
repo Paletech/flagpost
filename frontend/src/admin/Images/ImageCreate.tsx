daa036cd39de6d049a41bb91fcef12056821a960
OleksandrPrudyvus <pryduvyso20@gmail.com>
2023-03-23 11:06:58 +0200

    Created ImageUpload component

--- frontend/src/admin/Images/ImageCreate.tsx
+++ frontend/src/admin/Images/ImageCreate.tsx
@@ -1,20 +1,45 @@
-import React, { FC } from 'react';
+import React, {FC, useState} from 'react';
 import {
   Create,
   SimpleForm,
   ImageInput,
+    InputImage,
   ImageField,
 } from 'react-admin';
+import {BASE_URL} from "../../config";
+import {Button} from "@material-ui/core";
 
-export const ImageCreate: FC = (props) => (
-  <Create {...props}>
-    <SimpleForm>
+export const ImageCreate: FC = (props) => {
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
-    <ImageInput source="file" label="Related pictures" accept="image/*" placeholder={<p>Drop your file here</p>}>
-        <ImageField source="src" title="title" />
-    </ImageInput>
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setSelectedFile(event.target.files[0]);
+    }
+  };
 
+  const handleUpload = async () => {
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+      const token = localStorage.getItem("token")
+      const response = await fetch(BASE_URL + 'api/v1/images', {
+        method: 'POST',
+        headers: {
+          "Authorization": `Bearer ${token}`
+        },
+        body: formData
+      });
 
-    </SimpleForm>
-  </Create>
-);
+      if (!response.ok) {
+        throw new Error('Upload failed');
+      }
+    }
+  };
+
+    return (
+        <div>
+        <input type="file" onChange={handleFileChange} />
+        <Button onClick={handleUpload}>Upload</Button>
+      </div>)
+}
