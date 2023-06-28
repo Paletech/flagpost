391a4b1391c86ecab23d1c1ff2eb796d9bb46d07
Mykhailo Polishchuk <mpolishchuk86@gmail.com>
2023-06-28 14:37:36 +0300

    Changed main:app to app.main:app main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -70,4 +70,4 @@ handler = Mangum(app)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8888)
+    uvicorn.run("app.main:app", host="0.0.0.0", reload=True, port=8888)
