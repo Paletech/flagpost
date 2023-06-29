1d3565432db4cc615d051062c6dfc716695dcf4c
Mykhailo Polishchuk <mpolishchuk86@gmail.com>
2023-06-29 11:58:35 +0300

    Changed BASE_URL and BACKEND_URL front config

--- frontend/src/config/index.tsx
+++ frontend/src/config/index.tsx
@@ -1,3 +1,3 @@
-export const BASE_URL: string = 'https://q6wrnmjhfdzejctgmxra5f5hxi0xsdnf.lambda-url.us-east-1.on.aws/';
+export const BASE_URL: string = 'https://hz6w2y2scbbxlpqxbwplduvhxy0xpcwq.lambda-url.eu-central-1.on.aws/';
 export const BACKEND_URL: string =
-  'https://q6wrnmjhfdzejctgmxra5f5hxi0xsdnf.lambda-url.us-east-1.on.aws/api/v1';
+  'https://hz6w2y2scbbxlpqxbwplduvhxy0xpcwq.lambda-url.eu-central-1.on.aws/api/v1';
